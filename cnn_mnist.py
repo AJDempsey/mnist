2b86e8c890081901f094296ad414927a8b31bb32
Anthony Dempsey <anthonyjdempsey@gmail.com>
2018-06-07 17:16:43 +0100

    Initial commit
    Adding initial commit for Convolutional Neural Network mnist tutorial from
    https://www.tensorflow.org/tutorials/layers

---
+++ cnn_mnist.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_imprt
+from __future__ import division
+from __future__ import print_function
+
+# imports
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Application logic here
+
+if __name__ == "__main__":
+	tf.app.run()
