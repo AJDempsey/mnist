d4113bdbe4167aacd3324cc0de5e331e5fcc4862
Anthony Dempsey <anthonyjdempsey@gmail.com>
2018-06-07 17:33:42 +0100

    Add first layers of network
    Added first layers of the CNN input, convolution, and max pooling

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -10,5 +10,22 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 # Application logic here
 
+def cnn_model_fn(features, labels, mode):
+	"""Model function for CNN."""
+	# Input Layer
+	input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+	
+	# Convolution Layer #1
+	conv1 = tf.layers.conv2d(
+		inputs=input_layer,
+		filters=32,
+		kernel_size=[5, 5],
+		padding="same",
+		activation=tf.nn.relu)
+		
+	# Pooling Layer #1
+	pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+	
+
 if __name__ == "__main__":
 	tf.app.run()
