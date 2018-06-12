eab91c05351a5abe1096997d8ab21c5496025e04
Anthony Dempsey <anthonyjdempsey@gmail.com>
2018-06-12 11:30:41 +0100

    Add main function to run graph
    Follow tutorial to write the code that will run the graph
    
    NOTE: The library code to read in the files is going to be depreciated.
    This code should be replaced with my own code to read the training and
    test sets

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -107,5 +107,50 @@ def cnn_model_fn(features, labels, mode):
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops
     )
 
+
+def main(unused_argv):
+    # Load training and eval data
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_data = mnist.train.images # Returns np.array
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = mnist.test.images  # Returns np.array
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+    # Create Estimator (a TensorFlow class for performing high-level model training, evaluation, and
+    # inference in depth walk through at https://www.tensorflow.org/get_started/custom_estimators )
+    # Pass the model we've built and a directory for model data (checkpoints)
+    mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir='C:\\temp\\checkpoint')
+
+    # Set up logging for predictions, this will show us that the CNN is still running.
+    # The Logging will give us the probabilities from the softmax layer of the CNN
+    # Key is the value to log out, the value is name of the Tensor in the graph (line 80ish)
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=50
+    )
+
+    # Train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x" : train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True
+    )
+    mnist_classifier.train(input_fn=train_input_fn,
+                           steps=20000,
+                           hooks=[logging_hook])
+
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False
+    )
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
+
+
 if __name__ == "__main__":
     tf.app.run()
